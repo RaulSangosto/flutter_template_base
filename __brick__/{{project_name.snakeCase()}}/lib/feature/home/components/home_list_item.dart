6d31820276e824259a5f5cea812e6ac0b942ed74
RaulSangosto <48175562+RaulSangosto@users.noreply.github.com>
2024-06-03 19:54:38 +0200

    Fix: wrong import and translations

--- __brick__/{{project_name.snakeCase()}}/lib/feature/home/components/home_list_item.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/feature/home/components/home_list_item.dart
@@ -1,4 +1,4 @@
-import 'package:example_project_name2/core/model/test/test_model.dart';
+import '../../../core/model/test/test_model.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
